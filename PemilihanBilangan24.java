83a5340d45fcd97b6b36f90b7d23210f26affc92
Raihanbujeee <bujexixi@gmail.com>
2024-09-26 19:36:27 +0700

    day

--- PemilihanBilangan24.java
+++ PemilihanBilangan24.java
@@ -7,9 +7,8 @@ public class PemilihanBilangan24 {
         System.out.print("Masukkan angka: ");
         int angka = sc.nextInt();
         
-        // Menggunakan ternary operator untuk pemilihan ganjil/genap
         String hasil = (angka % 2 == 0) ? "angka " + angka + " termasuk genap" : "angka " + angka + " termasuk ganjil";
-        
+
         System.out.println(hasil);
     }
 }
