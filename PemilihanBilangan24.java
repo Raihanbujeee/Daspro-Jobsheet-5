4d0b3649ffe331750d0d9bd239cae585f2c48855
Raihanbujeee <bujexixi@gmail.com>
2024-09-26 18:53:50 +0700

    mod

--- PemilihanBilangan24.java
+++ PemilihanBilangan24.java
@@ -3,17 +3,13 @@ import java.util.Scanner;
 public class PemilihanBilangan24 {
 
     public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Masukkan angka: ");
-    int angka = sc.nextInt();
-    if (angka % 2 == 0 ) 
-    {
-      System.out.println("angka" + angka + " termasuk genap");  
-      
-    } 
-    else
-    {
-        System.out.println("angka"+ angka + "termasuk ganjil");
-    }   
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan angka: ");
+        int angka = sc.nextInt();
+        
+        // Menggunakan ternary operator untuk pemilihan ganjil/genap
+        String hasil = (angka % 2 == 0) ? "angka " + angka + " termasuk genap" : "angka " + angka + " termasuk ganjil";
+        
+        System.out.println(hasil);
     }
 }
